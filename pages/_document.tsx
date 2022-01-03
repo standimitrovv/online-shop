0f10541975f01cf07386ca4ee2e87a68b6048219
sharpzzy <evlution63@gmail.com>
2022-01-03 18:50:25 +0200

    Not loading font - fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,11 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          {/* <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' /> */}
           <link
             href='https://db.onlinewebfonts.com/c/b46bb1fc76216f5cd90457d0451dbee4?family=Futura+Book'
             rel='stylesheet'
-            type='text/css'
           />
           <link
             href='https://fonts.googleapis.com/css2?family=Montserrat&family=Nunito&family=Permanent+Marker&family=Playfair+Display&family=Satisfy&display=swap'
